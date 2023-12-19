8ab67c7d611ae802a57d30a2f5fd8c4c4bb6f433
Luis Gutierrez <luisgutierrez26489@gmail.com>
2023-12-18 19:37:42 -0600

    Added cels

--- main/main.rb
+++ main/main.rb
@@ -1,31 +1,46 @@
 # frozen_string_literal: true
 #! 
 class GameLife
-  def initialize(cols, rows)
+  def initialize(cols, rows, cels)
     @cols = cols
     @rows = rows
+    @cels = cels
     @tablero = crear_tablero
   end
 
   def crear_tablero
     # Crear el tablero con las dimensiones proporcionadas
-    Array.new(@rows) { Array.new(@cols, '.') }
+    tablero = Array.new(@rows) { Array.new(@cols, '.') }
+    @cels.times
+      tablero[rand(0..@cols)][rand(0..@rows)] = '*'
+    return tablero
   end
 
   def mostrar_tablero
     @tablero.each { |row| puts row.join(' ') }
   end
 
-  def generar_celula; end
+  def generar_celula
+    random1 = rand(0..@cols)
+    random2 = rand(0..@rows)
+
+    @tablero[random1][random2] = "*"
+
+    Array.new(@rows) { Array.new(@cols, '.') }
+
+  end
 
   def crear_vida; end
 end
 
 
 puts 'Por favor, ingresa la cantidad de columnas:'
-    cols = gets.chomp.to_i
+  cols = gets.chomp.to_i
 puts 'Ahora, ingresa la cantidad de filas:'
-    rows = gets.chomp.to_i
+  rows = gets.chomp.to_i
 
-tablero = GameLife.new(cols, rows)
+puts 'Cuantas celulas quieres generar'
+  cels= gets.chomp.to_i
+
+tablero = GameLife.new(cols, rows, cels)
 tablero.mostrar_tablero
